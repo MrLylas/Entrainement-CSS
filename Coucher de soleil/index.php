b306dc42999321e4eb41258c83c36a1b7e632625
MrLylas <delmas.denilson.pro@gmail.com>
2024-11-20 20:34:03 +0100

    first commit

---
+++ Coucher de soleil/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coucou</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <div class="circle soleil"></div>
+        <div class="circle lune"></div>
+        <div class="ciel"></div>
+        <div class="plage"></div>
+    </body>
+</html>
